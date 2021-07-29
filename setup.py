e7634af0081ecf11bab3d689bd65417bb67c8497
scheckley <stephen@CER-PC1N2M5T.Certara.com>
2021-07-29 15:52:21 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ setuptools.setup(
     version='1.0',                       
     author='Stephen Checkley',                    
     author_email='scheckley@gmail.com',
-    url='',
+    url='https://github.com/scheckley/pyvivc',
     description="numerical deconvolution and convolution methods working for inequal and incompatible timepoints between impulse and response curves for application in IVIVC level A",
     long_description=long_description,      
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),    
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: GPL :: Level 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],                                      
     python_requires='>=3.9',      
     py_modules=['pyvivc'],            
     package_dir={'':'pyvivc/src'},   
-    install_requires=['scipy, pandas, numpy'],
+    install_requires=['scipy', 'pandas', 'numpy'],
     project_urls={
-        'Bug Reports': ''
+        'Bug Reports': 'https://github.com/scheckley/pyvivc'
     }   
 )
